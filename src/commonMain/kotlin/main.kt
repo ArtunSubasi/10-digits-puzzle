afdbc8798aae1fee1cd8d72ba469dfccf8310216
Artun Subasi <asubasi@gmail.com>
2020-08-15 23:31:28 +0200

    Add some key handling to the main method for test purposes

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,10 +1,44 @@
+import com.soywiz.korev.Key
 import com.soywiz.korge.Korge
+import com.soywiz.korge.input.onKeyDown
 import com.soywiz.korge.view.text
 import com.soywiz.korim.color.Colors
-import org.gamezeug.digitspuzzle.domain.PuzzleAreaFactory
-import org.gamezeug.digitspuzzle.domain.PuzzleStateFactory
+import org.gamezeug.digitspuzzle.domain.*
 
 suspend fun main() = Korge(width = 1000, height = 512, bgcolor = Colors["#2b2b2b"]) {
 	val state = PuzzleStateFactory.createInitialPuzzleState()
-	text(state.toString())
+
+	val stateView = text(state.toString())
+
+	var x = 0
+	var y = 0
+	var waitingForY = false
+
+	onKeyDown {
+		if (waitingForY) {
+			y = getIntFromKey(it.key)
+			state.area = state.area.replaceTiles(TileReplacement(PuzzleAreaCoordinate(x, y), fullTile()))
+			stateView.text = state.area.toString()
+		} else {
+			x = getIntFromKey(it.key)
+		}
+		waitingForY = !waitingForY
+		println("X: $x, Y: $y")
+	}
+}
+
+private fun getIntFromKey(key: Key): Int {
+	return when (key) {
+		Key.N0 -> 0
+		Key.N1 -> 1
+		Key.N2 -> 2
+		Key.N3 -> 3
+		Key.N4 -> 4
+		Key.N5 -> 5
+		Key.N6 -> 6
+		Key.N7 -> 7
+		Key.N8 -> 8
+		Key.N9 -> 9
+		else -> 0
+	}
 }
