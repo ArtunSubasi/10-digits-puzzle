d5890acc2f73deeed3c2344b3c2885d9515fdfa2
Artun Subasi <asubasi@gmail.com>
2020-08-15 23:39:56 +0200

    Create a way to choose a piece

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -6,25 +6,40 @@ import com.soywiz.korim.color.Colors
 import org.gamezeug.digitspuzzle.domain.*
 
 suspend fun main() = Korge(width = 1000, height = 512, bgcolor = Colors["#2b2b2b"]) {
-	val state = PuzzleStateFactory.createInitialPuzzleState()
-
-	val stateView = text(state.toString())
-
+	val puzzleState = PuzzleStateFactory.createInitialPuzzleState()
+	var inputState = InputState.SELECT_PIECE
+	var piece = 0
 	var x = 0
 	var y = 0
-	var waitingForY = false
+
+	val stateView = text(puzzleState.toString())
+	println("Piece: $piece, X: $x, Y: $y, Next action: $inputState")
 
 	onKeyDown {
-		if (waitingForY) {
-			y = getIntFromKey(it.key)
-			state.area = state.area.replaceTiles(TileReplacement(PuzzleAreaCoordinate(x, y), fullTile()))
-			stateView.text = state.area.toString()
-		} else {
-			x = getIntFromKey(it.key)
+		when (inputState) {
+			InputState.SELECT_PIECE -> {
+				piece = getIntFromKey(it.key)
+				inputState = InputState.SELECT_X
+			}
+			InputState.SELECT_X -> {
+				x = getIntFromKey(it.key)
+				inputState = InputState.SELECT_Y
+			}
+			InputState.SELECT_Y -> {
+				y = getIntFromKey(it.key)
+				inputState = InputState.SELECT_PIECE
+				puzzleState.area = puzzleState.area.replaceTiles(TileReplacement(PuzzleAreaCoordinate(x, y), fullTile()))
+				stateView.text = puzzleState.area.toString()
+			}
 		}
-		waitingForY = !waitingForY
-		println("X: $x, Y: $y")
+		println("Piece: $piece, X: $x, Y: $y, Next action: $inputState")
 	}
+}
+
+enum class InputState {
+	SELECT_PIECE,
+	SELECT_X,
+	SELECT_Y
 }
 
 private fun getIntFromKey(key: Key): Int {
